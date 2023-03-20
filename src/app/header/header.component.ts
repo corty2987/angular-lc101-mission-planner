17bd28e7bf3b20f90a9bb0193ebf32ebb172feb7
corty2987 <cortyblackwell@gmail.com>
2023-03-20 13:47:29 -0500

    Finishing studio

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,15 +10,18 @@ export class HeaderComponent implements OnInit {
   missionName: string = "Mars 2030";
   rocketName: string = "Plasma Max";
   editingMission: boolean = false;
+  editingRocket: boolean = false;
   
   constructor() { }
 
   ngOnInit() {
   }
 
-  updateMission(updatedName: string) {
+  updateMission(updatedName: string, updatedRocket: string) {
     this.missionName = updatedName;
     this.editingMission = false;
+    this.rocketName = updatedRocket;
+    this.editingRocket = false;
   }
 
 }
